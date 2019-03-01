0d4bb9dea84ecafa470357e3c482f2c832440ae5
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2019-02-28 22:41:12 -0600

    fix bgs and font color

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { StaticQuery, graphql } from "gatsby";
 import styled from "styled-components";
 import "sanitize.css";
-// import Img from "gatsby-image";
-// import { Spring } from "react-spring/renderprops";
 
 import "./layout.css";
 
@@ -16,36 +13,28 @@ const MainLayout = styled.div`
   padding-top: ${props => (props.home ? "0" : "120px")};
   justify-content: ${props => (props.home ? "center" : "flex-start")};
   align-items: center;
-  background: ${props => (props.home ? "transparent" : "#F5F7FA")};
+  background: ${props => props.bg};
   @media (max-width: 600px) {
     padding-bottom: ${props => (props.home ? "20vh" : 0)};
     padding-top: ${props => (props.home ? "0" : "90px")};
   }
 `;
 
-const Layout = ({ children, location }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-        file(relativePath: { regex: "/bg/" }) {
-          childImageSharp {
-            fluid(maxWidth: 1000) {
-              src
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-      }
-    `}
-    render={() => (
-      <MainLayout home={location.pathname === "/"}>{children}</MainLayout>
-    )}
-  />
+const getBgColor = pathname => {
+  switch (pathname) {
+    case "/":
+      return "transparent";
+    case "/blog":
+      return "#f9eaeb";
+    default:
+      return "#F5F7FA";
+  }
+};
+
+const Layout = ({ children, location: { pathname } }) => (
+  <MainLayout bg={getBgColor(pathname)} home={pathname === "/"}>
+    {children}
+  </MainLayout>
 );
 
 Layout.propTypes = {
